6657925d18d3639d68fc3b5be232fb0c7fa46780
Vishnu <12vis7@gmail.com>
2020-06-04 17:37:05 +0530

    Added links for some, views, urls

--- src/urls.py
+++ src/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('members/', views.members, name='members'),
+    path('events/', views.events, name='events'),
+    path('read_more/', views.read_more, name='read_more')
 ]
--- src/views.py
+++ src/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'body': True})
+
+def about(request):
+    return render(request, 'index.html', {'about': True})
+
+def members(request):
+    return render(request, 'index.html', {'members': True})
+
+def events(request):
+    return render(request, 'index.html', {'events': True})
+
+def read_more(request):
+    return render(request, 'index.html', {'read_more': True})
